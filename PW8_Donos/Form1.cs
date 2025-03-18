29bd74dca46fce081eb00510dbf06c7dbf39d99d
315 <315@315-37>
2025-03-18 11:12:37 +0300

    Основа формы

--- PW8_Donos/Form1.cs
+++ PW8_Donos/Form1.cs
@@ -5,6 +5,18 @@ namespace PW8_Donos
         public Form1()
         {
             InitializeComponent();
+
+            button1.Click += button1_Click;
+        }
+
+        private void button1_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show("Привет");
+        }
+    
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
